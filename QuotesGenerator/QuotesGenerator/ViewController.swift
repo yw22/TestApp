1d8d5e9aefe996ae9dcc31a861e33bd2626c6c5d
김영욱 <uk@gim-yeong-ug-ui-MacBookAir.local>
2022-02-25 00:50:44 +0900

    init

---
+++ QuotesGenerator/QuotesGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuotesGenerator
+//
+//  Created by 김영욱 on 2022/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

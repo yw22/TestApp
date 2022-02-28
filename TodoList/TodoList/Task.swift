124f6a4689aba2d7f68cb0796421f4fe3e4b8bd2
yw22 <youngwook.dev@gmail.com>
2022-02-28 23:43:19 +0900

    Update

---
+++ TodoList/TodoList/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  TodoList
+//
+//  Created by 김영욱 on 2022/02/27.
+//
+
+import Foundation
+
+struct Task{
+  var title: String
+  var done: Bool
+  
+}
---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by 김영욱 on 2022/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var tableView: UITableView!
+  var tasks = [Task]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.tableView.dataSource = self
+    // Do any additional setup after loading the view.
+  }
+
+  @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+  }
+  
+  
+  @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+    let alert = UIAlertController(title: "할 일 등록", message: "할 일을 입력해주세요", preferredStyle: .alert)
+    let registerButton = UIAlertAction(title: "등록", style: .default, handler: { [weak self] _ in
+      guard let title = alert.textFields?[0].text else { return }
+      let task = Task(title: title, done: false)
+      self?.tasks.append(task)
+      self?.tableView.reloadData()
+    })
+    let cancelButton = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+    alert.addAction(cancelButton)
+    alert.addAction(registerButton)
+    alert.addTextField(configurationHandler: { textField in textField.placeholder = "할 일을 입력해 주세요."})
+    self.present(alert, animated: true, completion: nil)
+    
+  }
+}
+
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.tasks.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    let task = self.tasks[indexPath.row]
+    cell.textLabel?.text = task.title
+    return cell
+  }
+}

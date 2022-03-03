49cd0f6696c820fc39e841efc1b06cebc85a602e
yw22 <youngwook.dev@gmail.com>
2022-03-04 01:31:52 +0900

    Update

--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
   @IBOutlet weak var tableView: UITableView!
+  @IBOutlet var editButton: UIBarButtonItem!
+  var doneButton: UIBarButtonItem?
   var tasks = [Task]() {
     didSet {
       self.saveTasks()
@@ -18,13 +20,22 @@ class ViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTap))
     self.tableView.dataSource = self
     self.tableView.delegate = self
     self.loadTask()
     
   }
+  
+  @objc func doneButtonTap() {
+    self.navigationItem.leftBarButtonItem = self.editButton
+    self.tableView.setEditing(false, animated: true)
+  }
 
   @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+    guard !self.tasks.isEmpty else { return }
+    self.navigationItem.leftBarButtonItem = self.doneButton
+    self.tableView.setEditing(true, animated: true)
   }
   
   
@@ -81,7 +92,25 @@ extension ViewController: UITableViewDataSource {
     return cell
   }
   
+  func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    self.tasks.remove(at: indexPath.row)
+    tableView.deleteRows(at: [indexPath], with: .automatic)
+    if self.tasks.isEmpty {
+      self.doneButtonTap()
+    }
+  }
   
+  func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+  
+  func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+    var tasks = self.tasks
+    let task = tasks[sourceIndexPath.row]
+    tasks.remove(at: sourceIndexPath.row)
+    tasks.insert(task, at: destinationIndexPath.row)
+    self.tasks = tasks
+  }
 }
 
 extension ViewController: UITableViewDelegate {

6f95d6be83fa24b6232fde96f4052cc75b7a4a89
yw22 <youngwook.dev@gmail.com>
2022-03-04 10:59:30 +0900

    Swift Calculator init

---
+++ SwiftUICalculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 김영욱 on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

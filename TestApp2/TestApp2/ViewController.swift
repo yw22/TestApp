6eb7f7e86802fa3700429ce9d986683d8dcaf058
yw22 <youngwook.dev@gmail.com>
2022-04-09 22:22:54 +0900

    Dice Game

---
+++ TestApp2/TestApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp2
+//
+//  Created by 김영욱 on 2022/04/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
